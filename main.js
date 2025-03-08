2902a2ab2faf126c9a52b452627970ad00d5c36a
sudosign <imodulo.8@gmail.com>
2025-03-08 09:57:30 -0500

    class based

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
 const gridSize = 4;
 
-function createGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
-        var sect = document.createElement("section");
-        sect.className = `section section-${i}`; 
-        document.getElementById("container").appendChild(sect);
+function createBaseSquare() {
+    const div = document.createElement("div");
 
-        for (let j = 0; j < gridSize; j++) {
-            var div = document.createElement("div");
+    div.className = `square`; 
 
-            div.className = `square square-${j}`; 
+    div.style.margin = "0";
+    div.style.padding = "0";
+    div.style.border = "1px solid black";
+    div.style.flex = "1";
+    div.style.backgroundColor = "white";
 
-            div.style.margin = "0";
-            div.style.padding = "0";
-            div.style.border = "1px solid black"; 
-            div.style.flex = "1";
+    div.addEventListener("mouseover", function() {
+        this.style.backgroundColor = "whitesmoke";
+    });
+    div.addEventListener("mouseout", function() {
+        this.style.backgroundColor = "white";
+    });
+    div.addEventListener("mousedown", function() {
+        this.style.backgroundColor = "black";
+    });
 
-            div.style.backgroundColor = "white";
+    return div;
+}
 
-            div.addEventListener("mouseover", function() {
-                this.style.backgroundColor = "whitesmoke";
-            });
-            div.addEventListener("mouseout", function() {
-                this.style.backgroundColor = "white";
-            });
+function makeTargetSquare(square, gridSize) {
+    square.classList.add("target");
+    square.style.backgroundColor = "orange";
 
-            div.addEventListener("mousedown", function() {
-                this.style.backgroundColor = "black"; 
-            });
+    square.addEventListener("mouseover", targetMouseOver);
+    square.addEventListener("mouseout", targetMouseOut);
 
-            sect.appendChild(div);
-        }
-    }
-    const numOfTargets = Math.floor((gridSize * gridSize) / 4 - 0.01);
-    for (let i = 0; i < numOfTargets; i++) {
+    square.addEventListener("mousedown", function() {
+        this.style.backgroundColor = "white";
+        this.classList.remove("target");
+        this.removeEventListener("mouseover", targetMouseOver);
+        this.removeEventListener("mouseout", targetMouseOut);
+        this.removeEventListener("mousedown", arguments.callee); //change
         targetSquare(gridSize);
-    }
+    });
+}
+
+function targetMouseOver() {
+    this.style.backgroundColor = "#FFD580";
+}
+
+function targetMouseOut() {
+    this.style.backgroundColor = "orange";
 }
 
 function targetSquare(gridSize) {
@@ -46,45 +57,41 @@ function targetSquare(gridSize) {
         square = document.querySelector(`.section-${randomRow} .square-${randomSquare}`);
     } while (square && square.classList.contains("target"));
 
+    makeTargetSquare(square, gridSize);
+}
 
-    square.style.backgroundColor = "orange";
-    square.classList.add("target");
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        var sect = document.createElement("section");
+        sect.className = `section section-${i}`;
+        document.getElementById("container").appendChild(sect);
 
-    square.addEventListener("mouseover", function() {
-        this.style.backgroundColor = "#FFD580";
-    });
-    square.addEventListener("mouseout", function() {
-        this.style.backgroundColor = "orange";
-    });
-
-    square.addEventListener("mousedown", function() {
-        this.style.backgroundColor = "white"; 
-        this.replaceWith(this.cloneNode(true)); //godsend
+        for (let j = 0; j < gridSize; j++) {
+            const div = createBaseSquare();
+            div.classList.add(`square-${j}`);
+            sect.appendChild(div);
+        }
+    }
+    const numOfTargets = Math.floor((gridSize * gridSize) / 4 - 0.01);
+    for (let i = 0; i < numOfTargets; i++) {
         targetSquare(gridSize);
-    });
+    }
 }
 
 function clearGrid() {
-    // let gridSize;
-    // do {
-    //     gridSize = prompt("Please enter the new size of the grid:");
-    // } while (gridSize > 100);
-
     while (document.getElementById("container").firstChild) {
         document.getElementById("container").removeChild(document.getElementById("container").lastChild);
     }
-
-    // createGrid(gridSize);
-    createGrid(gridSize)
+    createGrid(gridSize);
 }
 
-createGrid(gridSize)
+createGrid(gridSize);
 
 const resetButton = document.querySelector("#clear-button");
 resetButton.onmousedown = clearGrid;
 
-document.addEventListener("keydown", function (event) {
-    if (event.key.toLowerCase() === "r") { // Checks if the 'R' key is pressed
+document.addEventListener("keydown", function(event) {
+    if (event.key.toLowerCase() === "r") { 
         clearGrid();
     }
 });
